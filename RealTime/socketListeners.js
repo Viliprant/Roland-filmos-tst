7500bd47d20d9962eb00a5d443fa8ac1e9484fb4
Viliprant <viliprant@gmail.com>
2021-02-19 19:35:51 +0100

    remove useless comments

--- RealTime/socketListeners.js
+++ RealTime/socketListeners.js
@@ -2,9 +2,7 @@ const random = require('string-random');
 
 module.exports =  (app) => {
     app.on('connection', connection => {
-        // console.log(connection.username);
-        // console.log(connection.connectionID);
-        // console.log(connection.payload);
+        app.channel('everybody').join(connection)
 
         if(!connection.payload){
             connection.payload = random(10);
@@ -15,7 +13,5 @@ module.exports =  (app) => {
         else{
             connection.payload = JSON.parse(connection.payload);
         }
-
-        app.channel('everybody').join(connection)
     });
 }
